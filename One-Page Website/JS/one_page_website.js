21679bc495e9d6d3f889c847a16b076cd6ba89b7
anett7 <anette.kurucz@gmail.com>
2023-05-11 20:49:29 +0100

    Testimonials
    Challenge 35

--- One-Page Website/JS/one_page_website.js
+++ One-Page Website/JS/one_page_website.js
@@ -59,3 +59,19 @@ function closeForm() {
   document.getElementById("myForm").style.display = "none";
 }
 // end open form on bottom of window
+
+//start of testimonials
+
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() { 
+  $('#slideshow > div:first')
+  .fadeOut(1000)
+  .next()
+  .fadeIn(1000)
+  .end()
+  .appendTo('#slideshow');
+}, 3000);
+
+
+//end of testimonials
